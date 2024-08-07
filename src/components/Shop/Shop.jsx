0d6963736c141206ea4ef8f41b0370a9f1b3f3e9
Mueen <hejiy2002@gmail.com>
2024-08-07 22:04:16 +0300

    m

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,48 @@
+
+import './Shop.css';
+// npm modules
+import { useState } from 'react';
+
+// components
+import InventoryList from '../../components/InventoryList/InventoryList';
+
+// data
+import { inventoryData } from '../../data/data';
+
+const Shop = () => {
+  // Create state variables for the shop and user inventories.
+  const [shopInventory, setShopInventory] = useState(inventoryData);
+  const [userInventory, setUserInventory] = useState([]);
+
+  const handleAddItem = (item) => {
+    setUserInventory([...userInventory, item]);
+    setShopInventory(shopInventory.filter((el) => el._id !== item._id));
+  };
+
+
+const handleRemoveItem = (item) => {
+    setShopInventory([...shopInventory, item]);
+    setUserInventory(userInventory.filter((el) => el._id !== item._id));
+  };
+ // src/components/Shop/Shop.jsx
+
+return (
+    <main>
+      <h1>Shop</h1>
+      <section className="shop-section">
+        <InventoryList
+          title="Shop Inventory"
+          inventory={shopInventory}
+          handleAddItem={handleAddItem}
+        />
+        <InventoryList
+          title="User Inventory"
+          inventory={userInventory}
+          handleRemoveItem={handleRemoveItem}
+        />
+      </section>
+    </main>
+  );
+};
+
+export default Shop;
